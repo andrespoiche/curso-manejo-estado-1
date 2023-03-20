1829f081e51cd994a564317555e74c56fb5d511d
andrespoiche <andrespoiche@gmail.com>
2023-03-20 17:52:12 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { UseState } from './UseState.js';
+import { ClassState } from './ClassState';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UseState/>
+      <ClassState/>
+    </div>
+  );
+}
+
+export default App;
